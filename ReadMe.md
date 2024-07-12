7d078ee8d9937b9053a488fc316c8e72b83f3c78
to4to <justjustifying@outlook.com>
2024-07-12 22:29:06 +0530

    Add enwiki-latest-abstract1.xml and Notes.txt to .gitignore

---
+++ ReadMe.md
@@ -0,0 +1,17 @@
+# Full Text Search Engine
+
+A Full Text Search Engine Capable To Search in Provided Document Upto 10M records.
+
+## Installation
+
+1. Clone Repository
+ ```
+ go get github.com/to4to/go-testsearch
+ ```
+ 2. Download Dependency
+
+ ```
+ go mod tidy
+ ```
+
+ 
---
+++ utils/document.go
@@ -0,0 +1,49 @@
+package utils
+
+import (
+	"compress/gzip"
+	"encoding/xml"
+	"os"
+)
+
+type document struct {
+	Title string `xml:"title"`
+	URL   string `xml:"url"`
+	Text  string `xml:"abstract"`
+	Id    int
+}
+
+func LoadDocuments(path string) ([]document, error) {
+
+	f, err := os.Open(path)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer f.Close()
+
+	gz, err := gzip.NewReader(f)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer gz.Close()
+
+	dec := xml.NewDecoder(gz)
+	dump := struct {
+		Documents []document `xml:"doc"`
+	}{}
+
+	if err := dec.Decode(&dump); err != nil {
+		return nil, err
+	}
+
+	docs := dump.Documents
+
+	for i := range docs {
+		docs[i].Id = i
+	}
+	return docs, nil
+}
