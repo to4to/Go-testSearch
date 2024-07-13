32a2231cbf05420dadf499363f922612f5d5eb39
to4to <justjustifying@outlook.com>
2024-07-14 00:13:17 +0530

    refactor: Add stop word filtering and stemming to token processing

--- utils/filter.go
+++ utils/filter.go
@@ -1,6 +1,10 @@
 package utils
 
-import "strings"
+import (
+	"strings"
+
+	snowballeng "github.com/kljensen/snowball/english"
+)
 
 // lowerCase converts all strings in the input slice to lowercase.
 func lowerCase(tokens []string) []string {
@@ -15,12 +19,29 @@ func lowerCase(tokens []string) []string {
 
 }
 
+// stopWordFilter removes stop words from a given list of tokens.
+// Stop words are common words that are often filtered out from text data.
+// It takes a slice of strings representing tokens and returns a new slice with stop words removed.
+func stopWordFilter(tokens []string) []string {
 
-func stopWordFilter(tokens []string) []string{
+	stopWords := map[string]struct{}{
+		"a": {}, "and": {}, "be": {}, "have": {}, "i": {},
+		"in": {}, "of": {}, "that": {}, "the": {}, "to": {},
+	}
 
+	r := make([]string, 0, len(tokens))
+	for _, token := range tokens {
+		if _, ok := stopWords[token]; !ok {
+			r = append(r, token)
+		}
+	}
+	return r
+}
 
-
-
-
-	
+func stemmerFilter(tokens []string) []string {
+	r := make([]string, len(tokens))
+	for i, token := range tokens {
+		r[i] = snowballeng.Stem(token, false)
+	}
+	return r
 }
